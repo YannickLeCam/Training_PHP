5d8f94d9026ed8e5262810877e4e640a8a250122
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-05-19 16:16:28 +0200

    testing d'une autre API avec code postal te retroune les informations concernant cette ville

--- TestAPI/City.php
+++ TestAPI/City.php
@@ -1,33 +1,32 @@
 <?php
-if (!isset($_POST["code_postal"])) {
-    $code_postal = 67000; //Paris par défault la capital
-}else {
+
+if (isset($_POST["code_postal"])) {
     $code_postal=(int)$_POST["code_postal"];
-}
 
-$curl = curl_init("http://api.zippopotam.us/fr/$code_postal");
-curl_setopt_array($curl,[
-    CURLOPT_CAINFO         => __DIR__ . DIRECTORY_SEPARATOR . 'cert.crt',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_CONNECTTIMEOUT => 1
-]);
-$data = curl_exec($curl);
-if ($data===false) {
-    var_dump(curl_error($curl));
-}else {
-    //On verifie que l'API ne renvoie pas un code d'erreur
-    if (curl_getinfo($curl,CURLINFO_RESPONSE_CODE)===200) {
-        $data = json_decode($data,true);
-        $city_name = $data["places"][0]["place name"];
-        $longitude = $data["places"][0]["longitude"];
-        $latitude = $data["places"][0]["latitude"];
-        $region = $data["places"][0]["state"];
+
+    $curl = curl_init("http://api.zippopotam.us/fr/$code_postal");
+    curl_setopt_array($curl,[
+        CURLOPT_CAINFO         => __DIR__ . DIRECTORY_SEPARATOR . 'cert.crt',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CONNECTTIMEOUT => 1
+    ]);
+    $data = curl_exec($curl);
+    if ($data===false) {
+        var_dump(curl_error($curl));
     }else {
-        //echo "Nous avons une erreur " . curl_error($curl) . " \nla reponse est : " . curl_getinfo($curl,CURLINFO_RESPONSE_CODE);
+        //On verifie que l'API ne renvoie pas un code d'erreur
+        if (curl_getinfo($curl,CURLINFO_RESPONSE_CODE)===200) {
+            $data = json_decode($data,true);
+            $city_name = $data["places"][0]["place name"];
+            $longitude = $data["places"][0]["longitude"];
+            $latitude = $data["places"][0]["latitude"];
+            $region = $data["places"][0]["state"];
+        }else {
+            //echo "Nous avons une erreur " . curl_error($curl) . " \nla reponse est : " . curl_getinfo($curl,CURLINFO_RESPONSE_CODE);
+        }
+
     }
-
 }
-
 ?>
 
 
@@ -36,13 +35,18 @@ if ($data===false) {
     <input type="number" name="code_postal" id="">
     <button type="submit">Envoyer</button>
 </form>
-<?php
-if (curl_getinfo($curl,CURLINFO_RESPONSE_CODE)===404) {?>
-   le code postal que vous avez selectionné ne semble ne pas etre valide ...
+
+<?php if (!isset($_POST["code_postal"])) {?>
+    Tapez un code postal ...
 <?php
 }else { ?>
+    <?php
+    if (curl_getinfo($curl,CURLINFO_RESPONSE_CODE)===404) {?>
+   le code postal que vous avez selectionné ne semble ne pas etre valide ...
+    <?php } else { ?>
     <p>Vous avez choisi le code postal <?= $code_postal ?> </p> <br>
     <p>La ville est <?= $city_name?> dans la region <?= $region ?> et la longitude est <?= $longitude ?> et la latitude est <?= $latitude?></p>
 <?php
+    }
 }
 ?>
